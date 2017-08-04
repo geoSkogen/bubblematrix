b156d4417a27f2bddf3cc894eae8e1eb695db130
joseph_scoggins <borninthecaul@gmail.com>
2017-08-03 20:56:30 -0700

    setup

---
+++ bubbleMatrix.js
@@ -0,0 +1,75 @@
+'use strict'
+
+window.addEventListener('load', initMatrix);
+
+function initMatrix() {
+  var ins = document.getElementsByClassName("matrixInput");
+  var buts = document.getElementsByClassName("matrixButton");
+  var conts = document.getElementsByClassName("matrixContainer");
+  var fields = document.getElementsByClassName("matrixFormField");
+  var list = [[]];
+  var string = "";
+  var test1 = document.getElementById("one")
+
+  function makeBubble(input, index) {
+    list[index].push(input)
+    var node = document.createTextNode(input);
+    var x = document.createTextNode('x');
+    var bubble = document.createElement('div');
+    var xIt = document.createElement('div');
+    var flexEnd = document.createElement('div');
+    bubble.className = "bubble";
+    xIt.className = "xIt";
+    flexEnd.className = "flexOuterEnd";
+    xIt.appendChild(x);
+    flexEnd.appendChild(xIt);
+    bubble.appendChild(flexEnd);
+    bubble.appendChild(node);
+    conts[index].appendChild(bubble);
+    string = list[index].join();
+    fields[index].value = string;
+    ins[index].value = "";
+    xIt.onclick = function () { unmakeBubble(bubble,index,input); };
+  }
+
+  function unmakeBubble(elm, index, input) {
+    conts[index].removeChild(elm);
+    var cutAt = list[index].indexOf(input);
+    list[index].splice(cutAt,1);
+    string = list[index].join();
+    fields[index].value = string;
+    ins[index].value = "";
+  }
+
+  function getInput(index) {
+    if (ins[index].value != "" && ins[index].value != " ") {
+      var inputVal = ins[index].value;
+      if (inputVal.indexOf(',') == -1) {
+        makeBubble(inputVal, index);
+      } else {
+        var inputArr = inputVal.split(',');
+        for (var i = 0; i < inputArr.length; i++) {
+          makeBubble(inputArr[i], index);
+        }
+      }
+    }
+  }
+
+  for (var i = 0; i < buts.length; i++) {
+    assignClick(buts[i],i);
+  }
+
+  function assignClick(button, index) {
+    button.onclick = function () {
+      getInput(index);
+    }
+  }
+
+  for (var i = 0; i < ins.length; i++) {
+    assignEnter(ins[i], i);
+  }
+
+  function assignEnter(input, index) {
+    input.addEventListener("keypress", function () { if (event.keyCode == 13) {getInput(index)}})
+  }
+}
